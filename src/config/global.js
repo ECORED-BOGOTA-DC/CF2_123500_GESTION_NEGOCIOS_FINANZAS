798ea81d0d91d692bfdf03f1252f6640dc9a6494
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2022-07-07 07:56:57 -0500

    avance 4

--- src/config/global.js
+++ src/config/global.js
@@ -119,31 +119,105 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
+      tema: 'Planeación estratégica ',
+      referencia:
+        'Torres, Z. y Torres, H. (2015). Planeación y control. Una visión integral de la administración. Grupo Editorial Patria.',
+      tipo: 'Libro',
+      link:
+        'https://elibro-net.bdigital.sena.edu.co/es/ereader/senavirtual/39408?page=1',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema: 'Planeación estratégica',
+      referencia:
+        'Núñez, A. (2014). Dirección de operaciones: decisiones tácticas y estratégicas. Editorial UOC.',
+      tipo: 'Libro',
+      link:
+        'https://elibro-net.bdigital.sena.edu.co/es/ereader/senavirtual/57659?page=616',
     },
   ],
   glosario: [
     {
-      termino: '',
-      significado: '',
+      termino: 'Administración estratégica',
+      significado:
+        'Es el proceso sinérgico que consiste en definir la naturaleza de la empresa, determinar la visión, misión, valores y objetivos, realizando un análisis interno y externo, para formular, implantar y desarrollar la estrategia, ponderar y retroalimentar a mediano y largo plazo. (Núñez, 2014)',
     },
     {
-      termino: '<em></em>',
-      significado: '',
+      termino: 'Diagnóstico empresarial ',
+      significado:
+        'Constituye una herramienta sencilla y de gran utilidad a los fines de conocer la situación actual de una organización y los problemas que impiden su crecimiento, sobrevivencia o desarrollo. Es un proceso de varios estudios realizados en las empresas de producción, servicios y de comercio.',
+    },
+    {
+      termino: 'Estrategia',
+      significado:
+        'Implica la determinación de las metas que se pretenden alcanzar en la empresa y que pueden hacer referencia a captar clientes, incrementar el beneficio o ganar competitividad, así como el establecimiento de una guía que oriente sobre cómo utilizar y combinar sus recursos y capacidades para el logro de sus objetivos. (Gómez, 2020)',
+    },
+    {
+      termino: 'FODA',
+      significado:
+        'Es una metodología utilizada para identificar el estudio de la situación competitiva de una empresa, a fin de determinar sus fortalezas, oportunidades, debilidades y amenazas. Su utilidad se refiere al diagnóstico de la situación interna y externa de la empresa, necesario antes de definir un plan estratégico. ',
+    },
+    {
+      termino: 'Misión',
+      significado:
+        'Representa la identidad y personalidad de cada empresa desde un punto de vista genérico. Responde a la pregunta sobre cuál es la esencia del negocio.',
+    },
+    {
+      termino: 'Modelo de negocio',
+      significado:
+        'Es una herramienta conceptual que contiene un conjunto de objetos, conceptos y las relaciones entre los mismos, con el objetivo de expresar la lógica de una empresa específica.',
+    },
+    {
+      termino: 'Objetivos',
+      significado:
+        'Resultados y logros que se desean alcanzar en un determinado tiempo.',
+    },
+    {
+      termino: 'Planeación',
+      significado:
+        'Es la función administrativa que determina anticipadamente los objetivos a alcanzar, así como lo que debe hacerse para alcanzarlos. La actividad incluye la previsión en el proceso administrativo. (Núñez, 2014)',
+    },
+    {
+      termino: 'Proceso estratégico',
+      significado:
+        'Proceso por el cual diseñamos cuáles son las líneas estratégicas de nuestro proyecto empresarial. Se definen cuáles son los planes de acción de las áreas, se implementan y se efectúa su seguimiento. (Gómez, 2020)',
+    },
+    {
+      termino: 'Recursos',
+      significado:
+        'Conjunto de medios o elementos que necesita una empresa para funcionar en forma efectiva.',
+    },
+    {
+      termino: 'Táctica',
+      significado:
+        'Planes de acción mediante los cuales se ponen en práctica las estrategias.',
+    },
+    {
+      termino: 'Ventaja competitiva',
+      significado:
+        'Constituye una destreza o habilidad especial que logra desarrollar una empresa y que la coloca en una posición de preferencia a los ojos del mercado.',
+    },
+    {
+      termino: 'Visión',
+      significado:
+        'Es el deseo de destino futuro que tiene la empresa en términos generales y que debe inspirar el futuro de la organización a largo plazo, responde a la pregunta en qué quiero que se convierta mi empresa. (Núñez, 2014)',
     },
   ],
   referencias: [
     {
-      referencia: '',
+      referencia:
+        'Gómez, I. y Brito, J. (2020). Administración de Operaciones. Universidad Internacional del Ecuador. ',
+      link:
+        'https://elibro-net.bdigital.sena.edu.co/es/ereader/senavirtual/131260?page=147 ',
+    },
+    {
+      referencia:
+        'Núñez, A. (2014). Dirección de operaciones: decisiones tácticas y estratégicas. Editorial UOC.',
+      link:
+        'https://elibro-net.bdigital.sena.edu.co/es/ereader/senavirtual/57659?page=616',
+    },
+    {
+      referencia:
+        'Sánchez, A. (2006). Peter Drucker, innovador maestro de la administración de empresas. Cuadernos Latinoamericanos de Administración, II(2), p. 69-89.',
       link: '',
     },
   ],
@@ -171,13 +245,30 @@ export default {
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
+        nombre: 'Leyson Fabian Castaño Perez',
+        cargo: 'Soporte organizacional',
+        centro:
+          'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+      },
+      {
         nombre: 'Nombre',
         cargo: 'Diseño web',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
+        nombre: 'Zuleidy María Ruíz Torres',
+        cargo: 'Producción audiovisual',
+        centro: 'Centro de Comercio y Servicios Regional Tolima',
+      },
+      {
+        nombre: 'Wilson Andrés Arenales Cáceres',
+        cargo: 'Producción audiovisual',
+        centro:
+          'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+      },
+      {
+        nombre: 'Gilberto Junior Rodríguez Rodríguez',
         cargo: 'Producción audiovisual',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',

cb9f4848b03c88725ac6f2e953fa13981e53827d
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2022-08-03 16:05:58 -0500

    Avance 7

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -864,7 +864,7 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/eFJOKJMVsQs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       
     .row.justify-content-center
       .col-md-8.col-10.mb-3.BG07.p-3
